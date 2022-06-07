6d0f3f589634c8afad03aa44b353c225c384c78a
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-07 16:04:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CONSUMED_BY_WORKERS = 26;
+            const int MINIMUM_SPICES_TO_GATHER_FROM_MINE = 100;
+            const int DAILY_DECREASE_OF_MINE_YIELD = 10;
+
+            int countSpices = int.Parse(Console.ReadLine());
+            int totalConsumed = 0;
+            int daysCounter = 0; 
+
+            while (countSpices >= MINIMUM_SPICES_TO_GATHER_FROM_MINE)
+            {
+                totalConsumed += countSpices - CONSUMED_BY_WORKERS;
+                countSpices -= DAILY_DECREASE_OF_MINE_YIELD;
+                daysCounter++;
+
+                if (countSpices < MINIMUM_SPICES_TO_GATHER_FROM_MINE)
+                {
+                    totalConsumed -= CONSUMED_BY_WORKERS;
+                }
+            }
+            Console.WriteLine(daysCounter);
+            Console.WriteLine(totalConsumed);
+
+            
+        }
+    }
+}
